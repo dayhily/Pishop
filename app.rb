ff5ff60db13b64268b3b58b7357eec6dd925e23f
Yura <yura@whatever.com>
2017-04-18 10:10:12 +0300

    Add Rakefile, update Gemfile, add model

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+#Устанавливаем соединение с БД barbershop.db
+set :database, "sqlite3:base.db"
+
+#Cоздание модели БД в классе Client
+class Programm_lang < ActiveRecord::Base
 
 get '/' do
 	erb :index
