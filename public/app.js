e62c704ef7296a4742107f8213d68237013af66e
Yura <yura@whatever.com>
2017-04-20 12:35:55 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,13 +1,7 @@
-  function something()
-  {
-    var x=window.localStorage.getItem('aaa'); //x = hh['aaa'], x-string type
-    x = x * 1 + 1; //x = x + 1, x*1 - make x from string to integer
-    window.localStorage.setItem('aaa',x); //hh['aaa'] = x
-         
-    alert(x); 
-  }
-  
   function add_to_cart(id)
   {
-    alert('You added pizza '+id);
-  }
+    var key = 'product_'+id;
+    var x = window.localStorage.getItem(key); //x = hh['key'], x-string type
+    x = x * 1 + 1; //x = x + 1, x*1 - make x from string to integer
+    window.localStorage.setItem(key, x); //hh['key'] = x
+  } 
