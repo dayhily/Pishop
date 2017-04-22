3e7c549e618e83fdf62e3381c4f09471a2b5a692
Yura <yura@whatever.com>
2017-04-22 12:07:43 +0300

    Update orders input field

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
-  function add_to_cart(id)
+  function add_to_cart(id) //формируем заказ из localStorage ввиде хэша
   {
     var key = 'product_'+id;
     var x = window.localStorage.getItem(key); //x = hh['key'], x-string type, получаем значение по ключу
     x = x * 1 + 1; //x = x + 1, x*1 - make x from string to integer
     window.localStorage.setItem(key, x); //hh['key'] = x
+    
+    update_orders();
   }
   
-  function cart_get_number_of_items()
+  function cart_get_number_of_items() //получаем количество товаров по ключу
   {
     var cnt = 0;
     for(var i = 0; i < window.localStorage.length; i++)
@@ -22,9 +24,13 @@
     return cnt;
   }
   
+  function update_orders() //обновляем заказ и отправляем в форму с id = "orders_input"
+  {
+    var orders = cart_get_orders();
+    $('#orders_input').val(orders);
+  }
   
-  
-    function cart_get_orders()
+  function cart_get_orders() //собираем заказ из localStorage в orders
   {
     var orders = '';
     for(var i = 0; i < window.localStorage.length; i++)
